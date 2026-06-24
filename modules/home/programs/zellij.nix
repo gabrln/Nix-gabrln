061f0ca663bd306dcaca87b7f2e4168462041c4c
Gabriel de Souza <gabrln@users.noreply.github.com>
2026-06-24 20:13:29 -0300

    feat(zellij): add custom config with vi-mode and tmux-prefix

--- modules/home/programs/zellij.nix
+++ modules/home/programs/zellij.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ lib, ... }:
 
 {
   programs.zellij = {
     enable = true;
+    # Using default config - custom config.kdl via mutable dotfile
   };
+
+  # Mutable dotfile for Zellij config
+  xdg.configFile."zellij/config.kdl".source =
+    lib.file.mkOutOfStoreSymlink
+      "${lib.home.homeDirectory}/.config/nixos/modules/home/dotfiles/zellij/config.kdl";
 }
